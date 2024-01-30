14da71ebea568b20ff251d0eaedd3a9b743355e8
Matias Bertuccio <tutebertuccio@hotmail.com>
2024-01-30 12:33:36 -0300

    Cambia las llamadas y la estructura del código
    Para un mejor rendimiento se decidió optar por ir iterando cada vez que el usuario quiera avanzar o ir a una página anterior

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-let limit = 600; // Limit => cantidad total de pokémones llamados
+let limit = 20; // Limit => cantidad total de pokémones llamados
 let offset = 0; // Offset => número desde el pokémon en que arrancará a llamar
-let infoPokemones;
-let indicePokemones = 0;
+let informacionPokemones;
 
-const API_URL = (`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`);
+obtenerLinksPokemones();
 
-fetch(API_URL)
-    .then(respuesta => respuesta.json())
-    .then(data => {
-        infoPokemones = data.results;
+function obtenerLinksPokemones() {
+    const API_URL = (`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`);
 
-        procesarDatos(infoPokemones);
-        mostrarPokemonesEnPantalla();
-    })
-    .catch(error => {
-        console.error("Error al obtener datos:", error);
-    })
 
-function procesarDatos(datos) {
-    Object.keys(datos).forEach((key) => {
-        guardarInformacionPokemon(datos[key].url);
-    })
+    fetch(API_URL)
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            informacionPokemones = data.results;
+            procesarInformacion(informacionPokemones);
+        })
+        .catch(error => {
+            console.error("Error al obtener datos:", error);
+        })
 }
 
-function guardarInformacionPokemon(urlPokemon) {
+function procesarInformacion(informacionPokemones) {
+    Object.keys(informacionPokemones).forEach((key) => {
+        llamarPokemon(informacionPokemones[key].url);
+    });
+}
+
+function llamarPokemon(urlPokemon) {
     fetch(urlPokemon)
         .then(respuesta => respuesta.json())
         .then(data => {
@@ -33,82 +35,68 @@ function guardarInformacionPokemon(urlPokemon) {
                 height: data.height,
                 weight: data.weight,
                 experience: data.base_experience,
-                sprite: data.sprites.front_default
+                sprite: data.sprites.front_default,
             };
-
+            mostrarPokemon(pokemon);
+            
             if (localStorage.getItem(pokemon.id) !== null) {
                 return;
             } else {
                 localStorage.setItem(`${pokemon.id}`, JSON.stringify(pokemon));
                 return;
             }
-
-            // Ejemplo para recuperar la información de un pokémon => const pokemonRecuperado = JSON.parse(localStorage.getItem("1"));
         })
 }
 
-function mostrarPokemonesEnPantalla() {
-    const $renglon1 = document.querySelector("#renglon-1");
-    const $renglon2 = document.querySelector("#renglon-2");
+function mostrarPokemon(pokemon) {
+    const $contenedorPokemones = document.querySelector("#contenedor-pokemones");
 
-    for(let i = 0; i <= 5; i++) {
-        indicePokemones++;
-        const pokemonRecuperado = JSON.parse(localStorage.getItem(`${indicePokemones}`));
+    const nombrePokemon = pokemon.name;
 
-        const nombrePokemon = pokemonRecuperado.name;
+    const $liPokemon = document.createElement("li");
+    $liPokemon.id = `${pokemon.id}`;
+    $liPokemon.classList = "col cuadro pokemon";
 
-        const $cuadroNuevo = document.createElement("div");
-        $cuadroNuevo.id = `${pokemonRecuperado.id}`;
-        $cuadroNuevo.classList = "col cuadro pokemon";
+    const $img = document.createElement("img");
+    $img.src = pokemon.sprite;
 
-        const $img = document.createElement("img");
-        $img.src = pokemonRecuperado.sprite;
+    const $h4 = document.createElement("h4");
+    $h4.textContent = nombrePokemon.charAt(0).toUpperCase() + nombrePokemon.slice(1);
 
-        const $h4 = document.createElement("h4");
-        $h4.textContent = nombrePokemon.charAt(0).toUpperCase() + nombrePokemon.slice(1);
+    $liPokemon.appendChild($img);
+    $liPokemon.appendChild($h4);
+    $contenedorPokemones.appendChild($liPokemon);
 
-        $cuadroNuevo.appendChild($img);
-        $cuadroNuevo.appendChild($h4);
-
-        $cuadroNuevo.addEventListener("click", mostrarInformacionPokemon);
-
-        if (i <= 2) {
-            $renglon1.appendChild($cuadroNuevo);
-        }
-
-        if (i >= 3) {
-            $renglon2.appendChild($cuadroNuevo);
-        }
-    }
+    $liPokemon.addEventListener("click", mostrarInformacionPokemon);
 }
 
 function mostrarInformacionPokemon(e) {
-    let pokemonMostrado = e.target;
+    let pokemonSeleccionado = e.target;
 
-    if (pokemonMostrado != "div") {
-        pokemonMostrado = e.target.parentNode;
+    if (pokemonSeleccionado != "div") {
+        pokemonSeleccionado = e.target.parentNode;
     }
 
-    const pokemonRecuperado = JSON.parse(localStorage.getItem(`${pokemonMostrado.id}`));
+    const pokemon = JSON.parse(localStorage.getItem(`${pokemonSeleccionado.id}`));
 
-    if (pokemonRecuperado === null) {
+    if (pokemon === null) {
         return;
     }
 
     abrirModal();
 
-    const nombrePokemon = pokemonRecuperado.name;
+    const nombrePokemon = pokemon.name;
 
     const $img = document.createElement("img");
-    $img.src = pokemonRecuperado.sprite;
+    $img.src = pokemon.sprite;
 
     const $contenedorPokemon = document.querySelector("#contenedor-img");
     $contenedorPokemon.appendChild($img);
 
     document.querySelector("#nombre-pokemon").textContent = nombrePokemon.charAt(0).toUpperCase() + nombrePokemon.slice(1);
-    document.querySelector("#altura-pokemon").textContent = `${pokemonRecuperado.height}`;
-    document.querySelector("#peso-pokemon").textContent = `${pokemonRecuperado.weight}`;
-    document.querySelector("#experiencia-pokemon").textContent = `${pokemonRecuperado.experience}`;
+    document.querySelector("#altura-pokemon").textContent = `${pokemon.height}`;
+    document.querySelector("#peso-pokemon").textContent = `${pokemon.weight}`;
+    document.querySelector("#experiencia-pokemon").textContent = `${pokemon.experience}`;
 }
 
 const $modal = document.querySelector(".modal");
@@ -135,34 +123,37 @@ function cerrarModal() {
     }
 }
 
+
 const $botonSiguiente = document.querySelector("#boton-siguiente");
 $botonSiguiente.onclick = mostrarPokemonesSiguientes;
 
 function mostrarPokemonesSiguientes() {
-    if (indicePokemones === 600) {
-        return false;
-    }
+    localStorage.clear();
+    offset += 20;
 
+    obtenerLinksPokemones();
     borrarPokemonesAnteriores();
-    mostrarPokemonesEnPantalla(indicePokemones);
 }
 
 const $botonAtras = document.querySelector("#boton-atras");
 $botonAtras.onclick = mostrarPokemonesAnteriores;
 
 function mostrarPokemonesAnteriores() {
-    if (indicePokemones === 6) {
+    if (offset === 0) {
         return false;
     }
 
+    localStorage.clear();   
+    offset -= 20;
+
+    obtenerLinksPokemones();
     borrarPokemonesAnteriores();
-    mostrarPokemonesEnPantalla(indicePokemones -= 12);
 }
 
 function borrarPokemonesAnteriores() {
     const $cuadros = document.querySelectorAll(".cuadro");
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 20; i++) {
         $cuadros[i].remove();
     }
 }
