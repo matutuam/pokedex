9fce3a5fd9287067697b29f67ed9efc42aa602b2
Matias Bertuccio <tutebertuccio@hotmail.com>
2024-03-26 10:23:39 -0300

    Agrega botones

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ const contenedorPokemones = document.querySelector('#contenedor-pokemones');
 let limit = 20;
 let offset = 1;
 
+const $botonAtras = document.querySelector('#boton-atras');
+$botonAtras.onclick = function () {
+    if (offset != 1) {
+        offset -= 20;
+        eliminarPokemonesAnteriores();
+        obtenerPokemones(offset, limit);
+    }
+}
+
+const $botonSiguiente = document.querySelector('#boton-siguiente');
+$botonSiguiente.onclick = function () {
+    offset += 20;
+    eliminarPokemonesAnteriores();
+    obtenerPokemones(offset, limit);
+}
+
+function eliminarPokemonesAnteriores() {
+    const pokemones = document.querySelectorAll('.pokemon');
+
+    for (let i = 0; i < pokemones.length; i++) {
+        pokemones[i].remove()
+    }
+}
+
 function obtenerPokemones(offset, limit) {
     for (let i = offset; i < offset + limit; i++) {
         obtenerPokemon(i);
@@ -10,7 +34,10 @@ function obtenerPokemones(offset, limit) {
 }
 
 function obtenerPokemon(id) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${window.indicePokemones}.png
+    // `https://pokeapi.co/api/v2/pokemon/${id}/`
+
+    fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`)
         .then((respuesta) => respuesta.json())
         .then((data) => {
             crearPokemon(data);
